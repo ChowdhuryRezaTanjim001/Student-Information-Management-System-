78047ed088a1c26ed4404447ec15598c92eb2ec5
Nsumaiya111 <118114604+Nsumaiya111@users.noreply.github.com>
2022-12-12 18:08:17 +0600

    Delete sql_files directory

--- sql_files/README.md
+++
@@ -1,2 +0,0 @@
-# Student-Information-Management-System-
-In this repository there will be a project called "Student Information Management System" where all the group members will work to develop a system
--- sql_files/payments.sql
+++
@@ -1,4 +0,0 @@
-create table Payments(student_id int (86),s_name varchar(86), course_fees  decimal(10,2), payment_type varchar(92),payable_balace decimal(8,4));
-insert into Payments(student_id,s_name ,current_balance,payment_type,payable_balace )
-values(2021487642,'tanjim', 4000,'done',548.34),
-(2021457642,'queen', 500,'not done',435.34);
