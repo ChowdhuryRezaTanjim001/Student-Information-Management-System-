89d848348c2eaa9afcff4fa88697744ee1aa3374
Nsumaiya111 <118114604+Nsumaiya111@users.noreply.github.com>
2022-12-12 15:10:45 +0600

    Delete admin.sql

--- sql_files/admin.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE  CREATE TABLE Admin ( student_id int (86) PRIMARY key, S_password varchar (95))
-insert into admin ( student_id, S_password)
-value(20214455,'jnfrenfu99'),
-(2021334455,'jnjndn9'),
-(202234555,'efefrfr99');
