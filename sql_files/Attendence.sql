36e9b08e23d653b80688955504bfa59560192230
Nsumaiya111 <118114604+Nsumaiya111@users.noreply.github.com>
2022-12-12 15:19:53 +0600

    Delete sql_files directory

--- sql_files/Attendence.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Attendence(student_id int (86),s_name varchar(86),course_name varchar(40), semester varchar(90),
-attendence boolean);
-insert into Attendence ( student_id,s_name,course_name,semester,attendence)
-values(2021488642,'queen','c005','4th',true),
-(2022578642,'anika','c003','6th',false);
--- sql_files/grade_history.sql
+++
@@ -1,1 +0,0 @@
-create table grade_history( student_id int(86),course_name varchar(40),grade varchar(40),course_id char(4));
