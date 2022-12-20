becc2a400e3de406a055ebc904b7c05c0ec29a59
ChowdhuryRezaTanjim001 <chowdhury.tanjim@northsouth.edu>
2022-12-21 01:34:45 +0600

    adding sql files

---
+++ sql_files/department.sql
@@ -0,0 +1,7 @@
+create TABLE Department (dep_name varchar(54) ,dep_code varchar(90) primary key, office_num int(90),dep_contact int (90),c_code varchar(40),f_id int(90));
+INSERT INTO department(dep_name,dep_code,office_num ,dep_contact,c_code,f_id)
+values( 'ECE','CSE',4455,01977888888,'CSE3111',204222),
+('Biotechonology','bio',46565,01786878878,'bio103',4555,2021488642),
+('ece','ece1',4455,0145578899,'cse3111',2044,193133604),
+('business','MBA',4355,01455786669,'his102',2444,34343344);
+
