dcc07f06c35e084c6162332c479655cd8062cdf1
Nsumaiya111 <118114604+Nsumaiya111@users.noreply.github.com>
2022-12-22 12:55:34 +0600

    Delete sql_files directory

--- sql_files/grading.sql
+++
@@ -1,7 +0,0 @@
-create table grading (grade varchar(5),cgpa float(8,3),student_id int(86),c_name varchar(20));
- 
-
-insert into grading(grade,cgpa,student_id,c_name)
-
-values ('A', 3.89,'2022487642','cse3111'),
- ('A-', 3.76,'2021488642','biotech11');
--- sql_files/student.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE  student( name varchar(86),fname varchar (86),Mname varchar (86), Lname varchar (86),student_id int (86),Address varchar (56),contact int(95),cname varchar(20),Enrolled_in date,dep_code varchar(90),	Email	varchar(255),password	varchar(255));
-
-insert into student(name,fname,Mname,Lname,student_id,Address,contact,Cname,Enrolled_in,dep_code,email,password)
-
-VALUES
-( 'tanjim','chowdhury','reza','tanjim',2021487642,'Mohammodpur road 123', 01310699957,'cse111', '2021-06-07','ece','tanjim@gmail.com','fffffff'),
- ('queen','nawal','queen','nafisa',2021488642,'Mirpur', 0131888888,'biotech11', '2021-08-07','bio','quenn@gmail.com','ftftft'),
- ("jhumu","jhumu","akther","ema",1931336042,'kuril',0176657677,'cse3111','2022-04-05','cse','jhumu@gmail.com','hhhhhh'),
-("sumona","mashruba","sumona","bristy",34343344,'uttora',0174455777,'his102','2022-05-09','MBA','sumona@gmailcom','hjhjhjh');
